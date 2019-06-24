ae6d253afb5e89afdd65b21555b070106d92b794
Chrystle <chrystle_myrna@yahoo.co.in>
2019-06-24 15:03:25 +0530

    Template update

--- gen_model.sh
+++ gen_model.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 if test -z $1 ;then
-	echo usage: $0 CLASSNUM
+	echo usage: $0 CLASSNUM PREFIX
         echo "        for voc the classnum is 21"
 	exit 1
 fi
 echo $1 |grep '^[0-9]*$' >/dev/null 2>&1
 if [ $? != 0 ];then
-	echo usage: $0 CLASSNUM
+	echo usage: $0 CLASSNUM PREFIX
         echo "        for voc the classnum is 21"
 	exit 1
 fi
+prefix=$2
 cls_num=$1
 cls_num3=$(expr $1 \* 3)
 cls_num6=$(expr $1 \* 6)
-trainfile=example/MobileNetSSD_train.prototxt
-testfile=example/MobileNetSSD_test.prototxt
-deploybnfile=example/MobileNetSSD_deploy_bn.prototxt
-deployfile=example/MobileNetSSD_deploy.prototxt
+trainfile=example/${prefix}_MobileNetSSD_train.prototxt
+testfile=example/${prefix}_MobileNetSSD_test.prototxt
+deploybnfile=example/${prefix}_MobileNetSSD_deploy_bn.prototxt
+deployfile=example/${prefix}_MobileNetSSD_deploy.prototxt
 
 mkdir -p example
 
